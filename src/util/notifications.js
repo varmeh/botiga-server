c55eed8cbfd8f02f58c13bdc311108eea02fce8d
varmehta <varun@botiga.app>
2020-10-27 16:53:08 +0530

    notifications api update

--- src/util/notifications.js
+++ src/util/notifications.js
@@ -25,14 +25,20 @@ const sendToUser = (pushToken, title, body) => {
 }
 
 const apartment = {
-	subscribeUser: (apartmentId, userToken) => {
-		admin.messaging().subscribeToTopic(userToken, `${apartmentId}_users`)
+	subscribe: ({ apartmentId, userToken, type = 'users' }) => {
+		if (type !== 'users' || type !== 'sellers') {
+			throw new Error('Invalid Type. Valid Values - users & sellers')
+		}
+		admin.messaging().subscribeToTopic(userToken, `${apartmentId}_${type}`)
 	},
-	send: (apartmentId, title, body) => {
+	send: ({ apartmentId, title, body, type = 'users' }) => {
+		if (type !== 'users' || type !== 'sellers') {
+			throw new Error('Invalid Type. Valid Values - users & sellers')
+		}
 		admin
 			.messaging()
 			.sendToTopic(
-				`${apartmentId}_users`,
+				`${apartmentId}_${type}`,
 				notificationPayload(title, body),
 				messagingOptions
 			)
