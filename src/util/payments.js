49ccf3544e16971e10a99cb31c904805bd732178
varmehta <varun@botiga.app>
2020-11-05 16:58:01 +0530

    paytm initiate transaction integration

---
+++ src/util/payments.js
@@ -0,0 +1,69 @@
+import axios from 'axios'
+import paytmChecksum from 'paytmchecksum'
+import { winston } from './winston.logger'
+
+const { PAYTM_HOST, PAYTM_MID, PAYTM_KEY, PAYTM_WEBSITE } = process.env
+
+/**
+ * Returns txnToken
+ */
+const initiateTransaction = async ({ txnAmount, orderNumber, customerId }) => {
+	try {
+		const paytmData = {
+			body: {
+				requestType: 'Payment',
+				mid: PAYTM_MID,
+				orderId: orderNumber,
+				websiteName: PAYTM_WEBSITE,
+				// callbackUrl: `${PAYTM_HOST}/theia/api/v1/paytm/callback?orderId=${orderNumber}`,
+				txnAmount: {
+					value: txnAmount,
+					currency: 'INR'
+				},
+				userInfo: {
+					custId: customerId
+				}
+			},
+			head: { channelId: 'WAP' }
+		}
+		paytmData.head.signature = await paytmChecksum.generateSignature(
+			JSON.stringify(paytmData.body),
+			PAYTM_KEY
+		)
+		const {
+			data: { head, body }
+		} = await axios.post(
+			`${PAYTM_HOST}/theia/api/v1/initiateTransaction?mid=${PAYTM_MID}&orderId=${orderNumber}`,
+			paytmData
+		)
+		if (body.resultInfo.resultStatus === 'S') {
+			// Validate Checksum
+			const isVerifySignature = await paytmChecksum.verifySignature(
+				JSON.stringify(body),
+				PAYTM_KEY,
+				head.signature
+			)
+
+			if (!isVerifySignature) {
+				winston.debug('@paytm checksum mismatched', {
+					error: 'paytm might be compromised. Verfiy'
+				})
+				throw new Error('Paytm Gateway Down. Please try again')
+			}
+		} else {
+			winston.debug('@paytm initiate transaction failed', {
+				error: body.resultInfo
+			})
+			throw Error(body.resultInfo.resultMsg)
+		}
+		return body.txnToken
+	} catch (error) {
+		winston.debug('@payment initiate transaction failed', {
+			error,
+			msg: error.message
+		})
+		return Promise.reject(new Error(error.message))
+	}
+}
+
+export default { initiateTransaction }
