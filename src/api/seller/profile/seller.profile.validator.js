902a628aa12022e21538466875a5eaaf792d54d3
varmehta <varun@botiga.app>
2020-12-01 15:59:26 +0530

    validation update

--- src/api/seller/profile/seller.profile.validator.js
+++ src/api/seller/profile/seller.profile.validator.js
@@ -1,20 +1,20 @@
 import {
 	phoneOptionalValidator,
-	addressOptionalValidator,
 	regexAlphaSpaceDigitsOptionalValidator,
 	pincodeOptionalValidator,
 	emailOptionalValidator,
 	emptyValidator,
 	boolValidator,
-	alphaNumericValidator
+	alphaNumericValidator,
+	emptyOptionalValidator
 } from '../../../util'
 
 export const patchContactValidator = [
 	emailOptionalValidator('email'),
 	phoneOptionalValidator('phone'),
 	phoneOptionalValidator('whatsapp'),
-	addressOptionalValidator('address.building'),
-	addressOptionalValidator('address.street'),
+	emptyOptionalValidator('address.building'),
+	emptyOptionalValidator('address.street'),
 	regexAlphaSpaceDigitsOptionalValidator('address.city'),
 	regexAlphaSpaceDigitsOptionalValidator('address.area'),
 	regexAlphaSpaceDigitsOptionalValidator('address.state'),
