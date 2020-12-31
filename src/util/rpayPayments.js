c770ae3369a095da9d140dda3005e51282544703
varmehta <varun@botiga.app>
2020-12-31 13:14:22 +0530

    rpayments refactor for test transactions

--- src/util/rpayPayments.js
+++ src/util/rpayPayments.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
-import { Order, PaymentStatus } from '../models'
+import { Order, PaymentStatus, User } from '../models'
 import { winston } from './winston.logger'
+
+const TEST_TRANSACTION = 'testTransaction'
 
 const { RPAY_HOST, RPAY_ID, RPAY_SECRET } = process.env
 
@@ -14,23 +16,23 @@ const initiateTransaction = async ({ txnAmount, orderId }) => {
 		// Save payment id for future reference
 		const order = await Order.findById(orderId)
 
-		const { data } = await axios.post(
-			`${RPAY_HOST}/orders`,
-			{
-				amount: txnAmount * 100,
-				currency: 'INR',
-				notes: { orderId: order.id }
-			},
-			{
-				headers: {
-					Authorization: `Basic ${authToken}`
-				}
-			}
-		)
+		const payload = {
+			amount: txnAmount * 100,
+			currency: 'INR',
+			notes: { orderId: orderId ?? TEST_TRANSACTION }
+		}
 
-		order.payment.status = PaymentStatus.initiated
-		order.payment.orderId = data.id
-		await order.save()
+		const { data } = await axios.post(`${RPAY_HOST}/orders`, payload, {
+			headers: {
+				Authorization: `Basic ${authToken}`
+			}
+		})
+
+		if (orderId) {
+			order.payment.status = PaymentStatus.initiated
+			order.payment.orderId = data.id
+			await order.save()
+		}
 
 		return data
 	} catch (error) {
@@ -43,13 +45,21 @@ const initiateTransaction = async ({ txnAmount, orderId }) => {
 }
 
 const webhook = async data => {
+	const {
+		event,
+		payload: {
+			payment: { entity }
+		}
+	} = data
+
 	try {
-		const {
-			event,
-			payload: {
-				payment: { entity }
-			}
-		} = data
+		if (!entity.notes.orderId || entity.notes.orderId === TEST_TRANSACTION) {
+			winston.info('@payment webhook test transaction', {
+				event,
+				entity
+			})
+			return
+		}
 		const order = await Order.findById(entity.notes.orderId)
 
 		if (event === 'payment.captured') {
@@ -60,15 +70,29 @@ const webhook = async data => {
 
 		order.payment.paymentId = entity.id
 		order.payment.paymentMode = entity.method
-		const updatedOrder = await order.save()
+		await order.save()
 
-		return updatedOrder
+		const user = await User.findById(order.buyer.id)
+
+		user.sendNotifications(
+			'Payment Update',
+			`Your payment of ${order.order.totalAmount} for order #${
+				order.order.number
+			} to ${order.seller.brandName} has ${
+				order.payment.status === PaymentStatus.success
+					? 'been successful'
+					: 'failed. Any amount debited will be credited back to your account.'
+			}`
+		)
 	} catch (error) {
 		winston.debug('@payment webhook failed', {
 			error,
-			msg: error.message
+			msg: error.message,
+			paymentId: entity.id,
+			event,
+			entity
 		})
-		return Promise.reject(new Error(error.message))
+		winston.error('@payment webhook failed', { paymentId: entity.id, event })
 	}
 }
 
