4fb22b9ae06b33a1de2211c7b7708208d87f12db
varmehta <varun@botiga.app>
2021-03-05 18:47:40 +0530

    manual cors fix

--- src/app.js
+++ src/app.js
@@ -22,6 +22,19 @@ configureApp(app)
 /* Configure Middlewares */
 app.use(rTracer.expressMiddleware())
 app.use(helmet())
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*') //* to give access to any origin
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization' //to give access to all the headers provided
+	)
+	if (req.method === 'OPTIONS') {
+		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET') //to give access to all the methods provided
+		return res.status(200).json({})
+	}
+	return next()
+})
+
 app.use(cors({ origin: true }))
 const fileFilter = (_, file, cb) => {
 	if (
